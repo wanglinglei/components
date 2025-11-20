5f7c1d24bc264130dc4df9acdeb1ad36085fd0f4
69994 <linglei.wang@bangdao-tech.com>
2025-11-20 15:25:51 +0800

    feat: 优化histoire文档示例

--- packages/docs/src/components/Button/Button.story.vue
+++ packages/docs/src/components/Button/Button.story.vue
@@ -77,5 +77,56 @@ const initialState = {
   disabled: false,
 };
 </script>
+<docs lang="md">
+# Button
 
+## 基础用法
+
+```vue
+<Button>默认按钮</Button>
+<Button type="primary">主要按钮</Button>
+<Button type="success">成功按钮</Button>
+<Button type="info">信息按钮</Button>
+<Button type="warning">警告按钮</Button>
+<Button type="danger">危险按钮</Button>
+```
+
+## 按钮尺寸
+
+```vue
+<Button size="large">大型按钮</Button>
+<Button>默认按钮</Button>
+<Button size="small">小型按钮</Button>
+```
+
+## 禁用状态
+
+```vue
+<Button disabled>默认按钮</Button>
+<Button type="primary" disabled>主要按钮</Button>
+<Button type="success" disabled>成功按钮</Button>
+```
+
+## API
+
+### Props
+
+| 参数     | 说明     | 类型    | 可选值                                                            | 默认值  |
+| -------- | -------- | ------- | ----------------------------------------------------------------- | ------- |
+| type     | 按钮类型 | string  | `primary` / `success` / `warning` / `danger` / `info` / `default` | default |
+| size     | 按钮尺寸 | string  | `large` / `default` / `small`                                     | default |
+| disabled | 是否禁用 | boolean | `true` / `false`                                                  | false   |
+
+### Events
+
+| 事件名 | 说明           | 回调参数            |
+| ------ | -------------- | ------------------- |
+| click  | 点击按钮时触发 | (event: MouseEvent) |
+
+### Slots
+
+| 名称    | 说明     |
+| ------- | -------- |
+| default | 按钮文本 |
+</docs>
 <style></style>
